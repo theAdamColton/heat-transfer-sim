0151b4486cd97fee9828eeedca614b9f2cc502c0
AdamC <atcolton@tutanota.com>
2021-09-19 00:50:56 -0600

    Removed lifetimes and cross entity references, filled out step function for Pipe and Tank

--- simulator/src/entities.rs
+++ simulator/src/entities.rs
@@ -2,6 +2,9 @@
 //! Defines structs to hold thermodynamic entities to be simulated.
 
 use std::f64::consts::PI;
+const WATER_SPECIFIC_HEAT: f64 = 4181.3f64; // J / K
+const WATER_DIFFUSIVITY: f64 = 1.4e-7; // m^2 / second
+const WATER_CONDUCTIVITY: f64 = 0.5918; // W / m * Kelvin
 
 pub trait Entity {
     /// Updates this entity's tempurature over a timestep delta_t
@@ -9,7 +12,7 @@ pub trait Entity {
 }
 
 /// Represents a pipe at a specific point in time
-pub struct Pipe<'a> {
+pub struct Pipe {
     /// The current temperature of the Pipe surrounding, in Kelvin
     pub temp: f64,
     /// The mass in Kg
@@ -26,48 +29,54 @@ pub struct Pipe<'a> {
     /// This is in units of W/ (m*K)
     pub thermal_cond: f64,
     /// The fluid that is contained in this pipe
-    pub fluid : &'a Fluid,
+    pub water: Water,
 }
 
-/// Represents an amount of Fluid at a specific point in time
-/// Fluid is not modeled by units of mass. Instead, changes in
+/**
+ * Calculate the heat transfer rate (W) at a specific point in time
+ * between a Pipe and a constant temperature fluid, then applies the 
+ * heat change to the pipe temperature and the water temperature.
+ */
+impl Entity for Pipe {
+    fn step(&mut self, delta_t: f64) {
+        // The heat transfer coefficient of the pipe interior wall
+        let h_int: f64 = self.thermal_cond / (self.rad_ext - self.rad_int);
+        // The heat transfer coefficient of the pipe exterior wall and the
+        // surrounding material
+        let h_ext: f64 = 2f64;
+        let heat_rate = 2f64 * PI * self.length * (self.water.temp - self.temp)
+            / (1f64 / (h_int * self.rad_int)
+                + (1f64 / self.thermal_cond) * (self.rad_ext / self.rad_int).ln()
+                + 1f64 / (h_ext * self.rad_ext));
+
+        let delta_q = heat_rate * delta_t;
+
+        self.temp = calculate_temp(delta_q, self.mass, self.specific_heat);
+        self.water.temp = calculate_temp(delta_q, self.water.mass, WATER_SPECIFIC_HEAT);
+    }
+}
+
+/// Represents an amount of Water at a specific point in time
 /// temperature are calculated from the flow_rate.
 /// All fluid is assumed to exhibit turbulent flow.
-pub struct Fluid {
+#[derive(Copy, Clone)]
+pub struct Water {
     /// Current temperature in Kelvin
     pub temp: f64,
-    /// The specific_heat in J / kg * K
-    pub specific_heat: f64,
     /// Mass of this unit of water in kg
     pub mass: f64,
 }
 
-/// Calculates the temperature change over a time unit delta_t
-/// temp = q (J) / (m (kg) * c (J/ kg * K) )
-pub fn calculate_temp(mass: f64, specific_heat: f64, delta_q: f64) -> f64 {
-    delta_q / (mass * specific_heat)
-}
-
-/// Calculate the heat transfer rate (W) at a specific point in time
-/// between a Pipe and a constant temperature fluid.
 /// TODO Equation in write up
-pub fn calculate_pipe_heat_rate(pipe: &Pipe, fluid: &Fluid) -> f64 {
-    // The heat transfer coefficient of the pipe interior wall
-    let h_int: f64 = pipe.thermal_cond / (pipe.rad_ext - pipe.rad_int);
-    // The heat transfer coefficient of the pipe exterior wall and the
-    // surrounding material
-    let h_ext: f64 = 2f64;
-    2f64 * PI * pipe.length * (fluid.temp - pipe.temp)
-        / (1f64 / (h_int * pipe.rad_int)
-            + (1f64 / pipe.thermal_cond) * (pipe.rad_ext / pipe.rad_int).ln()
-            + 1f64 / (h_ext * pipe.rad_ext))
+pub fn calculate_pipe_heat_rate(pipe: &Pipe, water: &Water) -> f64 {
+    0f64
 }
 
 /// Represents a liquid tank
 /// For simplicity, the internal liquid temperature
 /// is only modelled as the average temperature of all of the liquid
 /// in the tank
-pub struct Tank<'a> {
+pub struct Tank {
     /// Mass of dry tank in kg
     pub mass: f64,
     pub height: f64,
@@ -77,28 +86,90 @@ pub struct Tank<'a> {
     /// Avgr temp of the dry tank K
     pub temp: f64,
     /// Represents the fluid that is contained in this pipe
-    pub fluid: &'a Fluid,
-    /// Represents the pipe that is connected to the input
-    /// to this tank
-    pub pipe_in: &'a Pipe<'a>,
-    /// Represents the pipe that is connected to the output of
-    /// this Tank
-    pub pipe_out: &'a Pipe<'a>,
+    pub water: Water,
 }
 
-impl<'a> Entity for Tank<'a> {
+impl Entity for Tank {
+    /**
+     * Calculates the change in temperature to the tank, and to the water in the tank based on the
+     * current heat_transfer_coefficient. This implementation only facors in the heat loss from
+     * convection to the sides of the cylinder. A better way to do this would add the heat loss
+     * from the bottom and top of the cylinder, which would need to be calculated individually.
+     */
     fn step(&mut self, delta_t: f64) {
+        // Calculates the rayleigh number
+        let r_a = get_rayleigh(self.temp, self.height, &self.water);
+        // Calculates the Prandtl number
+        // https://www.tec-science.com/mechanics/gases-and-liquids/prandtl-number/
+        let p_r = water_viscosity(self.water.temp) / WATER_DIFFUSIVITY;
+        // Calculates the N_u Nusselt number
+        // Using equation for vertical plate convection
+        let n_u = 0.825
+            + 0.381 * r_a.powf(1f64 / 6f64)
+                / (1f64 + (0.492 / p_r).powf(9f64 / 16f64)).powf(8f64 / 27f64);
+        // Calculates the heat transfer coefficient h from the Nusselt number
+        let h = n_u * WATER_CONDUCTIVITY / self.height;
 
+        // Since the heat_transfer_coefficient only applies to vertical plate convection
+        // I only take the surface_area of the side of the tank.
+        let surface_area = 2f64 * PI * self.radius * self.height;
+
+        let heat_rate = h * surface_area * (self.water.temp - self.temp);
+
+        // Calculates the (sub) instantaneous heat change
+        let delta_q = heat_rate * delta_t;
+
+        // Applies the change to the temperatures
+        self.temp = calculate_temp(delta_q, self.mass, self.specific_heat);
+
+        self.water.temp = calculate_temp(delta_q, self.water.mass, WATER_SPECIFIC_HEAT);
     }
 }
 
-/// TODO Equation writeup
-/// Calculates the heat transfer rate at a specific point in time
-/// for the energy transfer between the tank and the enviroment.
-/// Returns the heat transfer rate in watts
-/// https://www.engineersedge.com/heat_transfer/cylinder_heat_transfer_buried_in_medium_13835.htm
-pub fn calculate_tank_heat_rate(tank: &Tank) -> f64 {
-    // Conduction shape factor
-    let s = 2f64 * PI * tank.height / (2f64 * tank.height / tank.radius).ln();
-    s * tank.fluid.specific_heat * (tank.temp - tank.fluid.temp)
+pub struct Panel {
+    pub mass: f64,
+    /// Area in m^2
+    pub area: f64,
+    pub temp: f64,
+    /// Input heat into the system
+    pub input_j: f64,
+    pub water: Water,
+    pub specific_heat: f64,
+    /// The heat transfer coeffient for this panel;
+    /// This can differ significantly based on
+    /// heat exchanger design.
+    pub heat_transfer_coefficient: f64,
+}
+
+impl Entity for Panel {
+    fn step(&mut self, delta_t: f64) {}
+}
+
+/// Get the heat rate between this panel and the fluid
+pub fn panel_heat_rate(panel: &Panel) -> f64 {
+    0f64
+}
+
+/// t_s is the temperature of the solid object,
+/// t_inf is the temperature of the fluid
+/// The thermal expansion coeff is assumed to be 1.
+/// TODO Equation
+fn get_rayleigh(t_s: f64, length: f64, water: &Water) -> f64 {
+    let viscosity = water_viscosity(water.temp);
+    9.81 * (t_s - water.temp) * length.powi(3) / (viscosity * WATER_DIFFUSIVITY)
+}
+
+/// Viscosity as a funct of temp of water
+/// This calculates the dynamic viscosity.
+/// For my purposes, I assume that the dynamic viscosity is
+/// the same as the kinematic viscosity.
+/// https://en.wikipedia.org/wiki/Viscosity#Water
+fn water_viscosity(temp: f64) -> f64 {
+    0.02939 * (507.88 / (temp - 149.3)).exp()
+}
+
+/// Calculates the temperature change over a time unit delta_t
+/// temp = q (J) / (m (kg) * c (J/ kg * K) )
+pub fn calculate_temp(delta_q: f64, mass: f64, specific_heat: f64) -> f64 {
+    delta_q / (mass * specific_heat)
 }
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -8,49 +8,54 @@ mod entities;
 use entities::*;
 
 fn main() {
-    let water_spec_heat = 4181.3f64;
     let steel_spec_heat = 466f64;
+    let steel_thermal_cond = 35f64;
 
-    let pipefluid = Fluid {
+    let pipewater = Water {
         mass: 4f64,
-        specific_heat: water_spec_heat,
         temp: 25f64,
     };
 
-    let tankfluid = Fluid {
+    let tankfluid = Water {
         mass: 300f64,
-        specific_heat: water_spec_heat,
         temp: 25f64,
     };
 
-    let steelpipe = Pipe {
+    let mut steelpipe = Pipe {
         length: 10f64,
         mass: 5f64,
         rad_int: 0.05f64,
         rad_ext: 0.07f64,
-        temp: 1f64,
+        temp: 25f64,
         specific_heat: 452f64,
         thermal_cond: 35f64,
-        fluid: &pipefluid,
+        water: pipewater,
     };
 
-    let tank = Tank {
+    let mut steelpipe_out = Pipe {
+        length: 10f64,
+        mass: 5f64,
+        rad_int: 0.05f64,
+        rad_ext: 0.07f64,
+        temp: 25f64,
+        specific_heat: steel_spec_heat,
+        thermal_cond: steel_thermal_cond,
+        water: pipewater.clone(),
+    };
+
+    let mut tank = Tank {
         mass: 40f64,
         height: 3f64,
         radius: 1f64,
         specific_heat: 400f64,
         temp: 22f64,
-        fluid: &tankfluid,
-        pipe_in: &steelpipe,
-        pipe_out: &steelpipe,
+        water: tankfluid,
     };
 
-    run_simulation(1e-6, 10f64, &steelpipe, &tank);
+    run_simulation(1e-6, 10f64, 0.5f64, &tank);
 }
 
 /**
  * Runs the simulation
  */
-fn run_simulation(delta_t: f64, input_watts: f64, pipe: &Pipe, tank: &Tank) {
-
-}
+fn run_simulation(delta_t: f64, input_watts: f64, pumprate: f64, tank: &Tank) {}

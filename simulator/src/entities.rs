39bc80b578c207bc0d5d1bfec813cefb1ed5d8ec
Adam <atcolton@tutanota.com>
2021-09-18 19:52:34 -0600

    Working on filling out structs

--- simulator/src/entities.rs
+++ simulator/src/entities.rs
@@ -2,30 +2,28 @@
 //! Defines structs to hold thermodynamic entities to be simulated.
 
 use std::f64::consts::PI;
-use f64::ln;
 
 pub trait Entity {
-    fn step(&mut self, delta_t : f64, delta_w : f64);
+    fn step(&mut self, delta_t: f64, delta_w: f64);
 }
-
 
 /// Represents a pipe at a specific point in time
 pub struct Pipe {
-    /// The current temperature of this entire Pipe, in Kelvin
-    temp: f64,
+    /// The current temperature of the Pipe surrounding, in Kelvin
+    pub temp: f64,
     /// The mass in Kg
-    mass: f64,
+    pub mass: f64,
     /// The interior radius in m
-    rad_int: f64,
+    pub rad_int: f64,
     /// Exterior radius in m
-    rad_ext: f64,
+    pub rad_ext: f64,
     /// Length of pipe in m
-    length: f64,
+    pub length: f64,
     /// The specific_heat in J / K
-    specific_heat: f64,
+    pub specific_heat: f64,
     /// Thermal conductivity
     /// This is in units of W/ (m*K)
-    thermal_cond: f64,
+    pub thermal_cond: f64,
 }
 
 /// Represents an amount of Fluid at a specific point in time
@@ -34,50 +32,61 @@ pub struct Pipe {
 /// All fluid is assumed to exhibit turbulent flow.
 pub struct Fluid {
     /// Current temperature in Kelvin
-    temp: f64,
+    pub temp: f64,
     /// The specific_heat in J / kg * K
-    specific_heat: f64,
+    pub specific_heat: f64,
     /// Mass of this unit of water in kg
-    mass: f64,
+    pub mass: f64,
 }
-
 
 /// Calculates the temperature change over a time unit delta_t
 /// temp = q (J) / (m (kg) * c (J/ kg * K) )
-fn calculate_temp(mass: f64, specific_heat: f64, delta_q: f64) -> f64 {
-    delta_q / ( mass * specific_heat )
+pub fn calculate_temp(mass: f64, specific_heat: f64, delta_q: f64) -> f64 {
+    delta_q / (mass * specific_heat)
 }
 
-
-/// Calculate the power transfer (W) at a specific point in time
+/// Calculate the heat transfer rate (W) at a specific point in time
 /// between a Pipe and a constant temperature fluid.
-///
 /// TODO Equation in write up
-fn calculate_pipe_heat_rate(pipe : &Pipe, fluid : &Fluid) -> f64 {
+pub fn calculate_pipe_heat_rate(pipe: &Pipe, fluid: &Fluid) -> f64 {
     // The heat transfer coefficient of the pipe interior wall
-    let h_int : f64 = pipe.thermal_cond / (pipe.rad_ext - pipe.rad_int);
-    // The heat transfer coefficient of the pipe exterior wall
-    // I'm assuming that nothing else exists in this scenerio except for the closed system
-    let h_ext : f64 = 0f64;
-    2f64 * PI * pipe.length * (fluid.temp - pipe.temp) / 
-        (1f64 / (h_int * pipe.rad_int) + (1f64 / pipe.thermal_cond) * ln(pipe.rad_ext / pipe.rad_int) + 1f64 / (h_ext * pipe.rad_ext))
+    let h_int: f64 = pipe.thermal_cond / (pipe.rad_ext - pipe.rad_int);
+    // The heat transfer coefficient of the pipe exterior wall and the
+    // surrounding material
+    let h_ext: f64 = 2f64;
+    2f64 * PI * pipe.length * (fluid.temp - pipe.temp)
+        / (1f64 / (h_int * pipe.rad_int)
+            + (1f64 / pipe.thermal_cond) * (pipe.rad_ext / pipe.rad_int).ln()
+            + 1f64 / (h_ext * pipe.rad_ext))
 }
 
+/// Represents a liquid tank
+/// For simplicity, the internal liquid temperature
+/// is only modelled as the average temperature of all of the liquid
+/// in the tank
+pub struct Tank {
+    /// Mass of dry tank in kg
+    mass: f64,
+    height: f64,
+    radius: f64,
+    /// Specific heat of dry tank J / kg * K
+    specific_heat: f64,
+    /// Mass of contained liquid in kg
+    liq_mass: f64,
+    /// Avgr temp of all of the liquid in the tank K
+    liq_avgr_temp: f64,
+    /// Specific heat of the liquid
+    liq_specific_heat: f64,
+    /// Avgr temp of the dry tank K
+    temp: f64,
+}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn demonstrate() {
-        let steelpipe = Pipe {
-            length : 10f64,
-            mass : 5f64,
-            rad_int : 0.05f64,
-            rad_ext : 0.07f64,
-            temp : 30f64,
-            specific_heat : 510f64,
-            thermal_cond : 35f64,
-        };
-    }
+/// TODO Equation writeup
+/// Calculates the heat transfer rate at a specific point in time
+/// Returns the heat transfer rate in watts 
+/// https://www.engineersedge.com/heat_transfer/cylinder_heat_transfer_buried_in_medium_13835.htm
+pub fn calculate_tank_heat_rate(tank : &Tank) -> f64 {
+    // Conduction shape factor
+    let s = 2f64 * PI * tank.height / (2f64 * tank.height / tank.radius).ln();
+    s * tank.liq_specific_heat * (tank.temp - tank.liq_avgr_temp)
 }
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -5,6 +5,32 @@
  */
 mod entities;
 
+use entities::*;
+
 fn main() {
-    println!("Hello, world!");
+        let steelpipe = Pipe {
+            length: 10f64,
+            mass: 5f64,
+            rad_int: 0.05f64,
+            rad_ext: 0.07f64,
+            temp: 1f64,
+            specific_heat: 452f64,
+            thermal_cond: 35f64,
+        };
+
+        let tank = Tank {
+            mass: 40f64,
+            height: 3f64,
+            radius: 1f64,
+            specific_heat: 400f64,
+            liq_mass: 1f64,
+            liq_specific_heat: 4182f64,
+            liq_avgr_temp: 22f64,
+            temp: 22f64,
+        };
+
+
+        let res = calculate_pipe_heat_rate(&steelpipe, &water);
+        println!("{}", res);
+
 }
